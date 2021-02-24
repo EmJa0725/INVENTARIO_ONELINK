6adbaed4e2f60694f22cf0485964a685f4fbbda8
EmJa0725 <67122190+EmJa0725@users.noreply.github.com>
2021-02-24 17:46:38 -0500

    carrousel images on signin page are resized for better performance on low resources machines

--- src/database.js
+++ src/database.js
@@ -1,26 +1,26 @@
 const { Console } = require('console');
-const mysql =  require('mysql');
+const mysql = require('mysql');
 const { promisify } = require('util');
 
-const {database} = require('./keys');
+const { database } = require('./keys');
 
 const pool = mysql.createPool(database);
 
-pool.getConnection((err, connection) =>{
-    if(err){
-        if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+pool.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
             console.error('DATABASE CONNECTION WAS CLOSED');
-         }
-         if (err.code === 'ER_CON_COUNT_EROOR') {
-             console.error('DATABASE HAS TO MANY CONNECTIONS');
-         }
-         if (err.code === 'ECONNREFUSED') {
-             console.error('DATABASE CONNECTION WAS REFUSED');
-         }
-    }    
+        }
+        if (err.code === 'ER_CON_COUNT_EROOR') {
+            console.error('DATABASE HAS TO MANY CONNECTIONS');
+        }
+        if (err.code === 'ECONNREFUSED') {
+            console.error('DATABASE CONNECTION WAS REFUSED');
+        }
+    }
     if (connection) connection.release()
-        console.log('DB is connected');
-        return;
+    console.log('DB is connected');
+    return;
 });
 
 pool.query = promisify(pool.query); // converts to promise functions
